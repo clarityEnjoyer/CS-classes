8e196b3aa441e5bdcc23610a03b09412d8ee3fc7
ok_i_pull_up <102764859+clarityEnjoyer@users.noreply.github.com>
2026-03-14 21:56:10 +0100

    Add files via upload

---
+++ FunctionalProgramming/lab1_26.02/churchNumbers.hs
@@ -0,0 +1,10 @@
+-- I can skip x's due to eta reduction!
+zero f x = x
+one f = f
+
+suc n f = f $ n f
+add n m f = n f $ m f
+mul n m f = n $ m f
+--function to convert church numbers to natural 
+tint n = n (+1) 0
+
